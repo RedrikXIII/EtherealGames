6a62f930137186390abeb5ebfaeab3ae6784d59e
Redrik_XIII <arsenii.rykov@gmail.com>
2023-11-12 00:03:38 +0300

    Animation, automatic scroll, a bit buggy click switch

---
+++ EtherealGames/Models/SliderModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EtherealGames.Models
+{
+    public class SliderModel
+    {
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ EtherealGames/Shared/Slider/SliderCard.razor.js
@@ -0,0 +1,39 @@
+﻿export class SliderCard
+{
+    static AddClick(component, slide) {
+        slide.addEventListener("click", () => { component.invokeMethodAsync("Click") });
+    }
+
+    static SetTimeout(component, slide) {
+        let mousenter = false;
+        let timeout = false;
+
+        function MouseEnter() {
+            mousenter = true;
+        }
+
+        function MouseLeave() {
+            mousenter = false;
+            if (timeout) {
+                Next();
+            }
+        }
+
+        function Next() {
+            slide.removeEventListener("mouseleave", MouseLeave);
+            slide.removeEventListener("mouseenter", MouseEnter);
+            component.invokeMethodAsync("Next");
+        }
+
+        slide.addEventListener("mouseenter", MouseEnter)
+        slide.addEventListener("mouseleave", MouseLeave)
+
+        setTimeout(() => {
+            timeout = true;
+            console.log(1);
+            if (!mousenter) {
+                Next();
+            }
+        }, 5000)
+    }
+}
