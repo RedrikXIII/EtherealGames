68d8d6c21e43ac2b5ebea57af7031e8cc96e73cb
Redrik_XIII <arsenii.rykov@gmail.com>
2023-11-17 18:00:01 +0300

    Modified Styles of mini-slider

--- EtherealGames/Data/DatabaseMoq.cs
+++ EtherealGames/Data/DatabaseMoq.cs
@@ -12,32 +12,38 @@ namespace EtherealGames.Data
                     new SliderModel()
                     {
                         Title = "Farlight 84",
-                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp"
+                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp",
+                        Url = "/games"
                     },
                     new SliderModel()
                     {
                         Title = "Test 1",
-                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp"
+                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp",
+                        Url = "/games"
                     },
                     new SliderModel()
                     {
                         Title = "Test 2",
-                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp"
+                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp",
+                        Url = "/games"
                     },
                     new SliderModel()
                     {
                         Title = "Test 3",
-                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp"
+                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp",
+                        Url = "/games"
                     },
                     new SliderModel()
                     {
                         Title = "Test 4",
-                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp"
+                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp",
+                        Url = "/games"
                     },
                     new SliderModel()
                     {
                         Title = "Test 5",
-                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp"
+                        Image = "https://cdn2.unrealengine.com/egs-farlight-84-carousel-thumb-1200x1600-1a3698d6350b.jpg?h=128&amp",
+                        Url = "/games"
                     },
                 };
 
@@ -45,5 +51,62 @@ namespace EtherealGames.Data
             } 
         }
 
+        public static List<GameModel> Games { get; set; }
+
+        static DatabaseMoq()
+        {
+            Games = new List<GameModel>()
+            {
+                new GameModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Price = 1200,
+                    Sale = 0,
+                    Title = "Game1"
+                },
+                new GameModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Price = 0,
+                    Sale = 0,
+                    Title = "Game2"
+                },
+                new GameModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Price = 999,
+                    Sale = 0,
+                    Title = "Game3"
+                },
+                new GameModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Price = 500,
+                    Sale = 50,
+                    Title = "Game4"
+                },
+                new GameModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Price = 1200,
+                    Sale = 33,
+                    Title = "Game5"
+                },
+                new GameModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Price = 120,
+                    Sale = 10,
+                    Title = "Game6"
+                },
+                new GameModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Price = 1200,
+                    Sale = 0,
+                    Title = "Game7"
+                }
+            };
+        }
     }
 }
---
+++ EtherealGames/Models/GameModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EtherealGames.Models
+{
+    public class GameModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public double Sale { get; set; }
+    }
+}
